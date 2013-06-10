c7c51671b853d9280896e655a49ab39b2d2c21b0
Ron Cohen <ronihcohen@gmail.com>
2013-06-10 13:06:16 +0300

    patients subgrid

--- application/views/showPatients.php
+++ application/views/showPatients.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-    <title>Codeigniter With JQGrid</title>
+    <title>Patients and diagnosis</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>jqGrid/css/redmond/jquery-ui-1.10.3.custom.css" />
@@ -19,17 +19,14 @@
 
 <body>
     
-        <h1>Patients</h1>
+        <h1>Patients and diagnosis</h1>
     <?php
         $ci =& get_instance();
         $base_url = base_url();
     ?>
     <table id="patients"></table><!--Grid table-->
     <div id="patients_pager"></div>  <!--pagination div-->
-
-    <table id="diagnosis"></table><!--Grid table-->
-    <div id="diagnosis_pager"></div>  <!--pagination div-->
-  
+   
 </body>
 
 
@@ -47,51 +44,41 @@
                 rowNum: 20,
                 width: 800,
                 search:false,
-           //     height: '100%',
-           //     rowList:[10,20,30],
-                pager: '#pager',
+                height: '100%',
+                rowList:[10,20,30],
+                pager: '#patients_pager',
                 sortname: 'id',
                 viewrecords: true,
                 rownumbers: true,
                 gridview: true,
                 editurl: '<?=$base_url.'index.php/patients/oper'?>',
                 caption:"Patients",
+                subGrid: true,
 
-                onSelectRow: function(ids) {
-                        jQuery("#diagnosis").jqGrid('setGridParam',{url:'<?=$base_url.'index.php/patients/diagnosisData/'?>'+ids});
-                        jQuery("#diagnosis").jqGrid('setCaption',"Diagnosis for patient "+ids)
-                        .trigger('reloadGrid');
-                    }
+                subGridRowExpanded: function(subgrid_id, row_id) {
+                    var subgrid_table_id, pager_id;
+                    subgrid_table_id = subgrid_id+"_t";
+                    pager_id = "p_"+subgrid_table_id;
+                    $("#"+subgrid_id).html("<table id='"+subgrid_table_id+"' class='scroll'></table>");
+                    jQuery("#"+subgrid_table_id).jqGrid({
+                        url:'<?=$base_url.'index.php/patients/diagnosisData/'?>'+row_id,
+                        datatype: "json",
+                        mtype : "post",
+                            colNames:['hebrew_name','english_name'],       //Grid column headings
+                            colModel:[
+                                {name:'hebrew_name',index:'hebrew_name', editable: false, required: false},
+                                {name:'english_name',index:'english_name', editable: false, required: false},
+                            ],
+                        sortname: 'num',
+                        sortorder: "asc",
+                        height: '100%'
+                    });
+                },
 
-            }).navGrid('#patients_pager',{edit:true,add:true,del:true,search:false});
+
+            }).navGrid('#patients_pager',{edit:true,add:true,del:true});
         });
 </script>
 
 
-<script type="text/javascript">
-        $(document).ready(function (){
-            jQuery("#diagnosis").jqGrid({
-                url:'<?=$base_url.'index.php/patients/diagnosisData/0'?>',      //another controller function for generating data
-                mtype : "post",             //Ajax request type. It also could be GET
-                datatype: "json",            //supported formats XML, JSON or Arrray
-                colNames:['hebrew_name','english_name'],       //Grid column headings
-                colModel:[
-                    {name:'hebrew_name',index:'hebrew_name', editable: false, required: false},
-                    {name:'english_name',index:'english_name', editable: false, required: false},
-                ],
-                rowNum: 20,
-                width: 800,
-           //     height: '100%',
-           //     rowList:[10,20,30],
-                pager: '#pager',
-                sortname: 'id',
-                viewrecords: true,
-                rownumbers: true,
-                gridview: true,
-                editurl: '<?=$base_url.'index.php/diagnosis/del'?>',
-                caption:"Diagnosis"
-
-            }).navGrid('#diagnosis_pager',{edit:false,add:false,del:false});
-        });
-</script>
 </html>
