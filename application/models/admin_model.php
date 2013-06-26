65007fab74e509028693f3aa4dff42bade59db28
Ron Cohen <ronihcohen@gmail.com>
2013-06-26 12:16:34 +0300

    users with group name

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -3,11 +3,11 @@
 class Admin_model extends CI_Model
 {
 	function getAllData($start,$limit,$sidx,$sord,$where){
-	    $this->db->select('*');
+	    $this->db->select('users.id as id,username,groups.id as group_id,groups.group_name');
 	    $this->db->limit($limit);
 	    if($where != NULL)
 	        $this->db->where($where,NULL,FALSE);
-	   // $this->db->order_by($sidx,$sord);
+	    $this->db->order_by($sidx,$sord);
 	    $this->db->join('groups', 'groups.id = users.group_id');
 	    $query = $this->db->get('users',$limit,$start);
 
@@ -31,7 +31,8 @@ class Admin_model extends CI_Model
 		$this->db->select('permissions.name');
 		$this->db->from('permissions_groups');
 		$this->db->join('permissions', 'permissions.id = permissions_groups.permission_id');
-		$this->db->where('permissions_groups.group_id',$id);
+		$this->db->join('users', 'users.group_id = permissions_groups.group_id');
+		$this->db->where('users.id',$id);
 
 	    $query = $this->db->get();
 
